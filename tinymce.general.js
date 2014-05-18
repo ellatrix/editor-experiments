76124cb391810c047b1f5b3e54357873d2216f07
avryl <jannekevandorpe@gmail.com>
2014-05-18 15:54:01 +0100

    Set ui colors based on content colors.

--- tinymce.general.js
+++ tinymce.general.js
@@ -3,9 +3,38 @@ tinymce.PluginManager.add( 'general', function( editor ) {
 
 	// Remove spaces from empty paragraphs.
 	editor.on( 'BeforeSetContent', function( event ) {
+
 		if ( event.content ) {
 			event.content = event.content.replace( /<p>(?:&nbsp;|\ufeff|\s)+<\/p>/gi, '<p></p>' );
 		}
+
+	} );
+
+	editor.on( 'init', function() {
+
+		function setColors() {
+
+			var style;
+
+			style = editor.dom.getStyle( editor.getBody(), 'color', true );
+			style = style.replace( /rgb\s*\(\s*([0-9]+)\s*,\s*([0-9]+)\s*,\s*([0-9]+)\s*\)/gi, function( match, r, g, b ) {
+				return 'rgba( ' + r + ', ' + g + ', ' + b + ', 0.8 )';
+			} );
+			style = '.wpview-wrap.selected { outline-color: ' + style + '; }';
+			style = editor.dom.create( 'style', { 'type': 'text/css' }, style );
+
+			editor.getDoc().head.appendChild( style );
+
+		}
+
+		jQuery( '#post-formats-select input.post-format' ).on( 'change.set-editor-class', function() {
+
+			setTimeout( setColors, 200 );
+
+		} );
+
+		setTimeout( setColors, 200 );
+
 	} );
 
 } );
