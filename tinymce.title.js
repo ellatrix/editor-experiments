746e144694ee8f1ee96f07cdfa8afffda25a0f7a
avryl <jannekevandorpe@gmail.com>
2014-05-26 17:30:32 +0100

    Clean up tinymce.title.js.

--- tinymce.title.js
+++ tinymce.title.js
@@ -2,28 +2,79 @@
 
 tinymce.PluginManager.add( 'title', function( editor ) {
 
-	var VK = tinymce.util.VK,
-		title;
+	var Env = tinymce.Env,
+		VK = tinymce.util.VK,
+		originalTitle = tinymce.DOM.select( '#title' )[0];
 
-	// editor.on( 'keydown', function( event ) {
-	// 	if ( event.keyCode === VK.BACKSPACE ) {
-	// 		event.stopPropagation();
-	// 	}
-	// } );
+	function dummyTitle() {
+		var title = editor.dom.select( '#wp-title' )[0],
+			body;
+		if ( ! title ) {
+			title = editor.dom.create(
+				'h1',
+				{ id: 'wp-title' },
+				originalTitle.value || ( ( Env.ie && Env.ie < 11 ) ? '' : '<br data-mce-bogus="1" />' )
+			);
+			body = editor.getBody();
+			body.insertBefore( title, body.firstChild );
+		}
+		return title;
+	}
 
-	editor.on( 'keyup', function( event ) {
-		jQuery( '#title' ).val( editor.dom.select( '#wp-title' )[0].textContent );
+	if ( ! originalTitle ) {
+		return;
+	}
+
+	editor.on( 'keydown', function( event ) {
+		var selection = editor.selection,
+			title, range, node;
+
+		if ( event.keyCode === VK.BACKSPACE ) {
+			range = selection.getRng();
+			node = selection.getNode();
+			title = dummyTitle();
+			if ( title &&
+					( node.previousSibling === title ||
+					node === title ) &&
+				selection.isCollapsed() && range.startOffset === 0 && range.endOffset === 0 ) {
+				event.preventDefault();
+			}
+		} else if ( event.keyCode === VK.ENTER ) {
+			node = selection.getNode();
+			if ( node === dummyTitle() ) {
+				selection.setCursorLocation( node.nextSibling );
+				event.preventDefault();
+			}
+		}
 	} );
 
-	editor.on( 'LoadContent', function() {
-		var body = editor.getBody();
-		title = editor.dom.create(
-			'h1',
-			{ id: 'wp-title' },
-			jQuery( '#title' ).val()
-		);
-		body.insertBefore( title, body.firstChild );
+	editor.on( 'nodechange blur', function() {
+		var dom = editor.dom,
+			title = dummyTitle(),
+			firstP = dom.select( 'p' )[0],
+			node = editor.selection.getNode();
+
+		if ( dom.isEmpty( title ) && node !== title ) {
+			dom.addClass( title, 'empty' );
+		} else {
+			dom.removeClass( title, 'empty' );
+		}
+
+		if ( dom.isEmpty( firstP ) && node !== firstP ) {
+			dom.addClass( firstP, 'empty' );
+		} else {
+			dom.removeClass( firstP, 'empty' );
+		}
 	} );
+
+	editor.on( 'keyup', function() {
+		var title = dummyTitle();
+		if ( title ) {
+			originalTitle.value = title.textContent;
+		}
+	} );
+
+	editor.on( 'LoadContent', dummyTitle );
 
 	editor.on( 'PostProcess', function( event ) {
 		if ( event.get ) {
@@ -32,11 +83,11 @@ tinymce.PluginManager.add( 'title', function( editor ) {
 	} );
 
 	editor.on( 'show BeforeRenderUI', function() {
-		jQuery( '#title' ).hide();
+		tinymce.DOM.hide( 'titlewrap' );
 	} );
 
 	editor.on( 'hide', function() {
-		jQuery( '#title' ).show();
+		tinymce.DOM.show( 'titlewrap' );
 	} );
 
 } );
